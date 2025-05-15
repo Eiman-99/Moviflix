58cb9292e138ae640a8ecdff1effad36c9914b46
unknown <eimanhamdy99@gmail.com>
2025-05-14 17:43:02 -0700

    Import swiper library

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 
-
 const getData = function(endpoint) {
   const API_KEY = 'cc687401dafd56a04490baaaa29e1329';
   const API_URL = 'https://api.themoviedb.org/3/';
@@ -12,3 +11,32 @@ const getData = function(endpoint) {
 };
 
 getData('discover/movie');
+
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  speed: 400,
+  spaceBetween: 10,
+  direction: 'horizontal',
+  loop: true,
+
+ 
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+   breakpoints: {
+      500: {
+        slidesPerView: 2,
+      },
+      700: {
+        slidesPerView: 4,
+      },
+      1200: {
+        slidesPerView: 6,
+      },
+    }
+
+ 
+});
