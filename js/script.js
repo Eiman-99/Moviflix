3e8714b782d6336b3c2c2afe54664ffee8aeb104
unknown <eimanhamdy99@gmail.com>
2025-05-14 19:35:27 -0700

    Render Swiper carousel with movie posters

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
+const pathName = window.location.pathname
+const swiper = document.querySelector('.swiper-wrapper')
+
+function initApp(){
+  if(pathName==='/'){
+    renderAll()
+  }
+}
+
 
 const getData = function(endpoint) {
   const API_KEY = 'cc687401dafd56a04490baaaa29e1329';
@@ -6,13 +15,32 @@ const getData = function(endpoint) {
   fetch(`${API_URL}${endpoint}?api_key=${API_KEY}`)
     .then(response => response.json())
     .then(data => {
-      console.log(data);
+      const {results} = data
+      console.log(results);
+      renderSwiper(results);
     }).catch(err=>console.log(err));
+
 };
 
-getData('discover/movie');
+// getData('discover/movie');
 
 
+function renderSwiper(data) {
+  let html = ` `;
+
+  data.forEach(item => {
+    html += `
+      <div class="swiper-slide">
+        <img src="https://image.tmdb.org/t/p/w500${item.poster_path}" alt="Movie poster" />
+      </div>
+    `;
+  });
+  swiper.innerHTML = html;  
+  initializeSwiper();    
+}
+
+
+function initializeSwiper(){
 const swiper = new Swiper('.swiper', {
   // Optional parameters
   speed: 400,
@@ -27,16 +55,27 @@ const swiper = new Swiper('.swiper', {
     prevEl: '.swiper-button-prev',
   },
    breakpoints: {
-      500: {
-        slidesPerView: 2,
+      375: {
+        slidesPerView: 3,
       },
-      700: {
+      500: {
         slidesPerView: 4,
       },
-      1200: {
+      700: {
         slidesPerView: 6,
+      },
+      1200: {
+        slidesPerView: 9,
       },
     }
 
  
 });
+}
+
+// render all suggestions on home page
+function renderAll(){
+  getData('discover/movie')
+}
+
+document.addEventListener('DOMContentLoaded', initApp);
